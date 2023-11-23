d865dd16d9215fbe1d0301d9220fbf2d6e5af302
Arthur Oliveira <arthurcnoliveira@gmail.com>
2023-11-23 16:10:07 +0000

    SECA-P1.5

--- seca-web-api.mjs
+++ seca-web-api.mjs
@@ -3,11 +3,18 @@ import secaServices from "./seca-services.mjs"
 
 const router = express.Router();
 
-router.get('/events/popular', secaServices.getPopularEvents);  //get the 30 most popular events
-router.get('/events/search/:eventName', secaServices.searchEvents);       //get an event by his name
-router.get('/groups', secaServices.getGroups);                 //get all the groups
-router.get('/groups/group/:groupId', secaServices.getGroup);            // get an especific group 
+router.get('/events/popular', secaServices.getPopularEvents);           //get the 30 most popular events
+router.get('/events/search/:eventName', secaServices.searchEvents);     //get an event by his name
+router.get('/groups', secaServices.getGroups);                          //get all the groups
+router.get('/groups/group/:groupId', secaServices.getGroup);            // get an especific group
+router.post('/groups', secaServices.postGroup);
+router.put('groups/group/:groupId/:type/:eventName', secaServices.editGroup);
+router.delete('groups/:groupsId', secaServices.deleteGroups);
+
+
 
 export default function secaWebApi(app){
     app.use('/api', router); 
 }
+
+//GET /api/event/popular => 
